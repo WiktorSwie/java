be254239bbad5a9e9e3154a6e9dbeb9712fc3294
WiktorSwie <115349384+WiktorSwie@users.noreply.github.com>
2023-06-15 17:54:48 +0200

    Add files via upload

---
+++ Frame.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+
+public class Frame extends JFrame{
+	Frame(){
+		this.setTitle("Aplikacja biblioteki");
+		this.setSize(500,500);
+		this.setVisible(true);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+	}
+	
+}
